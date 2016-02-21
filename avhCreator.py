a1b2659d23b96622a079a68557d6d527e2531a00
Hasan <rakibtg@gmail.com>
2016-02-21 13:03:16 +0600

    Added support for custom server directory

--- avhCreator.py
+++ avhCreator.py
@@ -17,26 +17,45 @@ def new_hosts(domain):
     # Chceck and set name of the public directory.
     if public_dir == "":
         public_dir = "public"
+    
+    newline()
+        
+    # Define the webserver parent directory
+    msg(" What would be the server parent directory? \n - Press enter to keep \"/var/www/\" as default location ")
+    server_parent_dir = raw_input()
+    if server_parent_dir == "":
+        server_parent_dir = "/var/www/"
+    else:
+        if os.path.exists(server_parent_dir) == False:
+            msg(" Parent directory (\""+server_parent_dir+"\") was not found! \n Please enter server parent directory again: ")
+            server_parent_dir = raw_input()
+        else:
+            msg(" Server parent directory has changed ")
 
     newline()
 
     msg(" Creating the Directory Structure ")
-    os.system("sudo mkdir -p /var/www/"+domain+"/"+public_dir)
+    os.system("sudo mkdir -p "+server_parent_dir+domain+"/"+public_dir)
 
     newline()
 
-    msg(" Granting Proper Permissions ")
-    os.system("sudo chown -R $USER:$USER /var/www/"+domain+"/"+public_dir)
+    msg(" Change directory permissions? \n It will give current user permission for this vhost and permit read access. \n If you want to change permission then type Y and press enter \n If you are not sure then press enter and skip this step")
+    uper = raw_input()
+    if (uper == "Y" or uper == "y"):
+        msg(" Granting Proper Permissions ")
+        os.system("sudo chown -R $USER:$USER "+server_parent_dir+domain+"/"+public_dir)
 
-    newline()
+        newline()
 
-    msg(" Making Sure Read Access is Permitted ")
-    os.system("sudo chmod -R 755 /var/www")
+        msg(" Making Sure Read Access is Permitted ")
+        os.system("sudo chmod -R 755 "+server_parent_dir+"/"+public_dir)
+    else:
+        msg( "Permission process skipped" )
 
     newline()
 
     msg(" Adding A Demo Page ")
-    file_object = open("/var/www/"+domain+"/"+public_dir+"/index.html", "w")
+    file_object = open(server_parent_dir+domain+"/"+public_dir+"/index.html", "w")
     file_object.write("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Virtual Hosts Created Successfully!</title><style>html{background-color: #508bc9;color: #fff;font-family: sans-serif, arial;}.container{width: 80%;margin: auto auto;}.inl{text-align: center;}.inl img{border-radius: 10px;}a{color: #f2d8ab;}</style></head><body><div class='container'><h1>Virtual Hosts Created Successfully!</h1><p><b>Apache Virtual Hosts Generator</b> has successfully created a virtual host in your server.<br>We can code it better! Join at <a href='https://github.com/rakibtg/Apache-Virtual-Hosts-Creator' target='_blank'>GitHub</a><br>Created by <a href='https://www.twitter.com/rakibtg' target='_blank'>Hasan</a></p><div class='divider'><div class='inl'><h1>Let's celebrate!</h1><img src='http://i.imgur.com/vCbBhwy.gif' alt='Scene from Spider Man Movie (C) Spider Man Movie ..'></div></div></div></body></html>")
     file_object.close()
 
@@ -44,7 +63,7 @@ def new_hosts(domain):
 
     msg(" Creating Virtual Host File ")
     host_file = open("/tmp/"+domain+".conf", "w")
-    host_file.write("<VirtualHost *:80>\nServerAdmin localserver@localhost\nServerName "+domain+"\nServerAlias www."+domain+"\nDocumentRoot /var/www/"+domain+"/"+public_dir+"\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
+    host_file.write("<VirtualHost *:80>\nServerAdmin localserver@localhost\nServerName "+domain+"\nServerAlias www."+domain+"\nDocumentRoot "+server_parent_dir+domain+"/"+public_dir+"\nErrorLog ${APACHE_LOG_DIR}/error.log\nCustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
     host_file.close()
     os.system("sudo mv \"/tmp/"+domain+".conf\" \"/etc/apache2/sites-available/\"")
 
